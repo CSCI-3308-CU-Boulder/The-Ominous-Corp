61b0563bb602139a7d38eb31ac007a11eabf3f4a
Jake <jcmartin987@gmail.com>
2020-09-21 18:03:40 -0600

    Added tank autorotation

--- All project code/UnityGames/Tank_Royale_Game/Assets/Movement.cs
+++ All project code/UnityGames/Tank_Royale_Game/Assets/Movement.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     Vector3 Vec;
+    Vector3 Rot;
+
+    float rotAng = 1.0F;
     float speed = 0.05F;
     // Start is called before the first frame update
     void Start()
@@ -34,5 +38,8 @@ public class Movement : MonoBehaviour
             Vec.x += speed;
         }
         transform.localPosition = Vec;
+
+
+        transform.Rotate(0, 0, rotAng, Space.Self);
     }
 }
